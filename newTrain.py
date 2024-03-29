0c6ff40913e112bd59931dfeee309c7e4e7d64d9
MD RAQIB KHAN <srk689800@gmail.com>
2024-03-30 04:06:31 +0530

    Train.py

--- newTrain.py
+++ newTrain.py
@@ -2,9 +2,7 @@ from __future__ import print_function
 import argparse
 import os
 from math import log10
-
-from utils import save_img,VGGPerceptualLoss,torchPSNR,ssim
-
+from utils import save_img,VGGPerceptualLoss
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -13,17 +11,13 @@ import torch.backends.cudnn as cudnn
 import numpy as np
 from PIL import Image
 import cv2
-import kornia
 from torchvision.transforms.functional import pad
 import torchvision.transforms as transforms
-from pytorch_msssim import MS_SSIM
-
-
-
-from network1 import define_G, define_D, GANLoss, get_scheduler, update_learning_rate,rgb_to_y,ContrastLoss
-from modifiedmodel1 import Restormer
+import torch.nn.functional as F
+from network1 import define_G, get_scheduler, update_learning_rate,rgb_to_y
+from modifiedmodel1 import Model
 from data import get_training_set, get_test_set
-from loss import Gradient_Loss,L1_Charbonnier_loss
+
 
 
 
@@ -32,7 +26,7 @@ parser = argparse.ArgumentParser(description='pix2pix-pytorch-implementation')
 parser.add_argument('--dataset', required=False, default = './uw_data/', help='facades')
 parser.add_argument('--batch_size', type=int, default=1, help='training batch size')
 parser.add_argument('--test_batch_size', type=int, default=1, help='testing batch size')
-parser.add_argument('--finetune', default=True, help='to finetune')
+parser.add_argument('--finetune', default=False, help='to finetune')
 parser.add_argument('--input_nc', type=int, default=3, help='input image channels')
 parser.add_argument('--output_nc', type=int, default=3, help='output image channels')
 parser.add_argument('--ngf', type=int, default=64, help='generator filters in first conv layer')
@@ -48,7 +42,7 @@ parser.add_argument('--cuda', action='store_true',default=True, help='use cuda?'
 parser.add_argument('--threads', type=int, default=0, help='number of threads for data loader to use')
 parser.add_argument('--seed', type=int, default=123, help='random seed to use. Default=123')
 parser.add_argument('--lamb', type=int, default=10, help='weight on L1 term in objective')
-parser.add_argument('--edge_loss', default=True, help='apply edge loss for training')
+parser.add_argument('--edge_loss', default=False, help='apply edge loss for training')
 parser.add_argument('--edge_loss_type', default='sobel', help='apply canny or sobel loss loss for training')
 opt = parser.parse_args()
 
@@ -65,21 +59,15 @@ if opt.cuda:
 
 device = torch.device("cuda:0" if opt.cuda else "cpu")
 
-
-if opt.finetune is True :
-    my_net = Restormer().to(device)
-    # net_g = define_G(opt.input_nc, opt.output_nc, opt.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
-    net_d = define_D(opt.input_nc + opt.output_nc, opt.ndf, 'basic', gpu_id=device)
+print('===> Building models')
+if opt.finetune is False :
+    my_net = Model().to(device)
     my_net = my_net.cuda(device = 0)
-    net_d = net_d.cuda(device = 0)
+    
 
 else:
     G_path = "checkpoint/uw_data/netG_model_epoch_2.pth".format(opt.epoch_count)
     my_net = torch.load(G_path).cuda(device = 0)
-
-    D_path = "checkpoint/uw_data/netD_model_epoch_2.pth".format(opt.epoch_count)
-    net_d = torch.load(D_path).cuda(device = 0)
-
 
   
 
@@ -91,103 +79,109 @@ training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, ba
 testing_data_loader = DataLoader(dataset=test_set, num_workers=opt.threads, batch_size=opt.test_batch_size, shuffle=False)
 
 
-print('===> Building models')
-# net_g = Unet(3,3)#define_G(opt.input_nc, opt.output_nc, opt.ngf, 'batch', False, 'normal', 0.02, gpu_id=device)
-# input_size = 256
-# # arch = Uformer
-# depths=[2, 2, 2, 2, 2, 2, 2, 2, 2]
-# net_g = Uformer(img_size=input_size, embed_dim=16,depths=depths,
-#                  win_size=8, mlp_ratio=4., token_projection='linear', token_mlp='leff', modulator=True, shift_flag=False)
-net_g= Restormer().to(device)
 
-net_d = define_D(opt.input_nc + opt.output_nc, opt.ndf, 'basic', gpu_id=device)
 
+class Gradient_Loss(nn.Module):
+    def __init__(self):
+        super(Gradient_Loss, self).__init__()
+
+        kernel_g = [[[0,1,0],[1,-4,1],[0,1,0]],
+                    [[0,1,0],[1,-4,1],[0,1,0]],
+                    [[0,1,0],[1,-4,1],[0,1,0]]]
+        kernel_g = torch.FloatTensor(kernel_g).unsqueeze(0).permute(1, 0, 2, 3)
+        self.weight_g = nn.Parameter(data=kernel_g, requires_grad=False)
+
+    def forward(self, x,xx):
+        grad = 0
+        y = x
+        yy = xx
+        gradient_x = F.conv2d(y, self.weight_g,groups=3)
+        gradient_xx = F.conv2d(yy,self.weight_g,groups=3)
+        l = nn.L1Loss()
+        a = l(gradient_x,gradient_xx)
+        grad = grad + a
+        return grad
+class CharbonnierLoss(nn.Module):
+    """Charbonnier Loss (L1)"""
+
+    def __init__(self, loss_weight=1.0, reduction='mean', eps=1e-3):
+        super(CharbonnierLoss, self).__init__()
+        self.eps = eps
+
+    def forward(self, x, y):
+        diff = x - y
+        loss = torch.mean(torch.sqrt((diff * diff) + (self.eps*self.eps)))
+        return loss  
+              
+######--------defie losses -----####
+
+Gradient_Loss=Gradient_Loss().to(device)
 criterionGAN = GANLoss().to(device)
 criterionL1 = nn.L1Loss().to(device)
 criterionMSE = nn.MSELoss().to(device)
-L_per = VGGPerceptualLoss().to(device)
-L1_loss = nn.L1Loss().cuda()
-
-MS_SSIM_loss = MS_SSIM(win_size=11, win_sigma=1.5, data_range=1, size_average=True, channel=3).to(device)
-Gradient_loss = Gradient_Loss().to(device)
-Charbonnier_loss =L1_Charbonnier_loss().to(device)
+L_per = VGGPerceptualLoss().to(device)  
 
 # setup optimizer
 optimizer_g = optim.Adam(net_g.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-optimizer_d = optim.Adam(net_d.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
 net_g_scheduler = get_scheduler(optimizer_g, opt)
-net_d_scheduler = get_scheduler(optimizer_d, opt)
+
 print('parameters of model are',sum(dict((p.data_ptr(), p.numel()) for p in net_g.parameters()).values()))
 for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
     # train
     for iteration, batch in enumerate(training_data_loader, 1):
         # forward
         rgb, tar , rgb_grey= batch[0].to(device), batch[1].to(device), batch[2].to(device)
-        # rgb=pad_img( rgb, (256,256))
-        # tar=pad_img(tar,(256,256))
-        # rgb_grey=pad_img(rgb_grey, (256,256))
-        # tar_grey=pad_img(tar_grey,(256,256))
-        # real_a, real_b = batch[0].to(device), batch[1].to(device)
-        # real_b = real_b.unsqueeze(0)
-        # real_a = real_a.unsqueeze(0)
-        # print(rgb.shape,rgb_grey.shape)
-        fake_b = net_g(rgb,rgb_grey)
-        fake_bY=rgb_to_y(fake_b)
-        rgb_Y = rgb_to_y(rgb)
-        tar_Y= rgb_to_y(tar)
-        ######################
-        # (1) Update D network
-        ######################
-
-        optimizer_d.zero_grad()
-        
-        # train with fake
-
-        
-        fake_ab = torch.cat((rgb, fake_b), 1)
-
-
-        pred_fake = net_d.forward(fake_ab.detach())
-        loss_d_fake = criterionGAN(pred_fake, False)
-        
-        # loss_con=L_vgg(fake_b[0], real_b[0])
-                     
-
-        # train with real
       
-        real_ab = torch.cat((rgb, tar), 1)
-        pred_real = net_d.forward(real_ab)
-        loss_d_real = criterionGAN(pred_real, True)
-
-        # real_ab1 = torch.cat((fake_b, real_b), 1)
-        # pred_real1 = net_d.forward(real_ab1)
-        # loss_per=L_vgg(pred_real1, True)
-        # Combined D loss
-        loss_d = (loss_d_fake + loss_d_real) * 0.5 
-
-        loss_d.backward()
-       
-        optimizer_d.step()
-
-        ######################
+        fake_b = my_net(rgb,rgb_grey) #model output
+        fake_bY=rgb_to_y(fake_b)
+        tar_Y= rgb_to_y(tar)
+   
+        #####################
         # (2) Update G network
         ######################
 
         optimizer_g.zero_grad()
 
-        # First, G(A) should fake the discriminator
-        fake_ab = torch.cat((rgb, fake_b), 1)
-        pred_fake = net_d.forward(fake_ab)
-        loss_g_gan = criterionGAN(pred_fake, True)
+   
         #same size as an input size
-        
-        # Second, G(A) = B
-        # phase_loss=criterionL1((torch.fft.fftn(fake_b)).angle(),(torch.fft.fftn(tar)).angle())
-        loss_msssim_SR = 1 - MS_SSIM_loss(fake_b, tar)
-        loss_g_l1 = 2*L1_Charbonnier_loss(fake_b[:,:,:,1], tar[:,:,:,1])+3*L1_Charbonnier_loss(tar_Y,fake_bY) +1.5*L_per(fake_b,tar)+5*loss_msssim_SR
+       
+        loss_g_l1 = criterionL1(fake_b[:,0,:,:],tar[:,0,:,:]) * opt.lamb+criterionL1(fake_b, tar)*5+15*criterionL1(tar_Y,fake_bY) +0.9*L_per(fake_b,tar)+1.9*(1-ssim(fake_b,tar,255))
         #####edg_loss###33
+        if opt.edge_loss is True:
 
-        loss_g = loss_g_gan*0.01  + loss_g_l1 
+            if opt.edge_loss_type == 'canny':
+
+                ################################################## Adding Edge Loss ##################################
+                edge_out1 = kornia.filters.canny(fake_b, low_threshold=0.1, high_threshold=0.2, kernel_size=(5, 5), sigma=(1, 1), hysteresis=True, eps=1e-06)
+                
+                edge_gt = kornia.filters.canny(tar, low_threshold=0.1, high_threshold=0.2, kernel_size=(5, 5), sigma=(1, 1), hysteresis=True, eps=1e-06)
+                
+                
+                
+                edge_loss_p1_magnitude = criterionL1(edge_out1[1], edge_gt[1]) 
+               
+            else:
+
+
+                ############################################### Sobel Loss ###########################################
+                edge_out1 = kornia.filters.sobel(fake_b, normalized=True, eps=1e-06)
+               
+                edge_gt = kornia.filters.sobel(tar, normalized=True, eps=1e-06)
+               
+                edge_loss_p1_magnitude = criterionL1(edge_out1[0], edge_gt[0]) 
+                
+
+
+            edge_loss = edge_loss_p1_magnitude 
+            
+            loss_g = loss_g_l1  + 1*edge_loss+Gradient_Loss(fake_b,tar)
+
+        else:
+            loss_g =loss_g_l1+Gradient_Loss(fake_b,tar)
+
+        
+
+        
         
         loss_g.backward()
 
@@ -196,53 +190,31 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         if iteration % 1000==0:
             out_image=torch.cat((rgb, fake_b,tar), 3)
             out_image = out_image[0].detach().squeeze(0).cpu()
-
-            # input_rgb = rgb[0].detach().squeeze(0).cpu()
-            # GT_rgb = tar[0].detach().squeeze(0).cpu()
             
             print("===> Epoch[{}]({}/{}): Loss_D: {:.4f} Loss_G: {:.4f} ".format(
                 epoch, iteration, len(training_data_loader), loss_d.item(), loss_g.item() ))
-            save_img(out_image, 'images/'+str(iteration)+'.png')
-
-            # save_img(input_rgb,'images/'+str(iteration)+'inp'+'.jpg')
-            # save_img1(out_image,'images/'+str(iteration)+'out'+'.jpg')
-            # save_img(GT_rgb,'images/'+str(iteration)+'GT'+'.jpg')
+            if not os.path.exists("train_images"):
+              os.mkdir("train_images")
+            save_img(out_image, 'train_images/'+str(iteration)+'.png')
 
 
     update_learning_rate(net_g_scheduler, optimizer_g)
-    update_learning_rate(net_d_scheduler, optimizer_d)
+
 
     # test
-    avg_psnr = 0
-    SSIM=0
+    
     for test_iter, batch in enumerate(testing_data_loader,1):
         rgb_input, target,grey_input = batch[0].to(device), batch[1].to(device), batch[2].to(device)
-        # input = input.unsqueeze(0)
-        # target = target.unsqueeze(0)
-        # rgb_input=pad_img(rgb_input, (256,256))
-        # target=pad_img(target,(256,256))
-        # grey_input=pad_img(rgb_grey, (256,256))
-        # grey_target=pad_img(grey_target,(256,256))
-        prediction = net_g(rgb_input,grey_input)
+        prediction = my_net(rgb_input,grey_input)
         out=torch.cat((rgb_input,prediction,target),3)
         output_cat =out[0].detach().squeeze(0).cpu()
-        # inp =rgb_input[0].detach().squeeze(0).cpu()
-        # out =prediction[0].detach().squeeze(0).cpu()
-        # GT  =target[0].detach().squeeze(0).cpu()
-        save_img(output_cat, 'images_test/'+str(test_iter)+'inp'+'.jpg')
-
-        # save_img(inp, 'images_test/'+str(test_iter)+'inp'+'.jpg')
-        # save_img1(out, 'images_test/'+str(test_iter)+'out'+'.jpg')
-        # save_img(GT, 'images_test/'+str(test_iter)+'GT'+'.jpg')
-    
-    #     SSim=ssim(fake_b,tar,255)
-    #     SSIM+=SSim
-        # SSim=torchSSIM(fake_b,tar)
-        # SSIM+=SSim
-        psnr= torchPSNR(prediction, target).item()
-        avg_psnr += psnr
-    print("===> Avg. PSNR: {:.4f}dB ".format(avg_psnr / len(testing_data_loader)))
-
+        if not os.path.exists("test_images"):
+            os.mkdir("test_images")
+        
+        save_img(output_cat, 'test_images/'+str(test_iter)+'inp'+'.jpg')
+       
+        
+        
     #checkpoint
     if epoch % 1== 0:
         if not os.path.exists("checkpoint"):
@@ -250,8 +222,5 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         if not os.path.exists(os.path.join("checkpoint", opt.dataset)):
             os.mkdir(os.path.join("checkpoint", opt.dataset))
         net_g_model_out_path = "checkpoint/{}/netG_model_epoch_{}.pth".format(opt.dataset, epoch)
-        net_d_model_out_path = "checkpoint/{}/netD_model_epoch_{}.pth".format(opt.dataset, epoch)
         torch.save(net_g, net_g_model_out_path)
-        
-        torch.save(net_d, net_d_model_out_path)
         print("Checkpoint saved to {}".format("checkpoint" + opt.dataset))

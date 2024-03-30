2bc414d0512b907bc1f68f88eae91d298af2102d
MD RAQIB KHAN <srk689800@gmail.com>
2024-03-30 17:24:58 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ import torch
 import torchvision 
 import torch.nn as nn
 from torch.nn import init
-
+import torch.nn.functional as F 
+import math
+import cv2
 
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in [".png", ".jpg", ".jpeg"])
@@ -24,12 +26,6 @@ def save_img(image_tensor, filename):
     image_pil = Image.fromarray(image_numpy)
     image_pil.save(filename)
     # print("Image saved as {}".format(filename))
-
-def torchPSNR(tar_img, prd_img):
-    imdff = torch.clamp(prd_img, 0, 1) - torch.clamp(tar_img, 0, 1)
-    rmse = (imdff**2).mean().sqrt()
-    ps = 20*torch.log10(1/rmse)
-    return ps
 
 class VGGPerceptualLoss(torch.nn.Module):
     def __init__(self, resize=True):
@@ -72,37 +68,6 @@ class VGGPerceptualLoss(torch.nn.Module):
                 gram_y = act_y @ act_y.permute(0, 2, 1)
                 loss += torch.nn.functional.l1_loss(gram_x, gram_y)
         return loss
-
-# def ssim(img1, img2):
-#     C1 = (0.01 * 255)**2
-#     C2 = (0.03 * 255)**2
-
-#     img1 = img1.astype(np.float64)
-#     img2 = img2.astype(np.float64)
-#     kernel = cv2.getGaussianKernel(11, 1.5)
-#     window = np.outer(kernel, kernel.transpose())
-
-#     mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
-#     mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
-#     mu1_sq = mu1**2
-#     mu2_sq = mu2**2
-#     mu1_mu2 = mu1 * mu2
-#     sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
-#     sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
-#     sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
-
-#     ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
-#                                                             (sigma1_sq + sigma2_sq + C2))
-#     return 1-ssim_map.mean()
-
-
-import torch  
-import torch.nn.functional as F 
-import numpy as np
-import math
-from PIL import Image
-import cv2
-
 
 def gaussian(window_size, sigma):
     """

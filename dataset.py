1bc21dad22e2f8e2912fe91e46b702958ad924ad
MD RAQIB KHAN <srk689800@gmail.com>
2024-03-30 16:56:34 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -13,6 +13,7 @@ def rgb2gray(rgb):
   r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
   gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
   return gray
+
 def transg(im):
   im=transforms.ToTensor()(im)
   im = transforms.Normalize((0.5),(0.5))(im)
